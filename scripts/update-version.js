98f156640a1673741a056e551198290a4f66fc18
raysaii <309406931@qq.com>
2019-03-08 18:40:42 +0800

    1.构建测试

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process')
+const { exec, execSync } = require('child_process')
 const { join } = require('path')
 const { readJson, writeJson } = require('fs-extra')
 
@@ -6,32 +6,28 @@ const run = async () => {
   const packageJSONPath = join('.', 'package.json')
   const packageJSON = await readJson(packageJSONPath)
   let version = packageJSON.version
-  const tep = version.split('.').map(num => parseInt(num))
-
-  const addVersion = (arr, str = '') => {
-    if (arr.length == 1) {
-      return arr[0] + 1 + str
+  const name = exec('git rev-parse --abbrev-ref HEAD').toString().trim()
+  if (name == 'dev') {
+    const tep = version.split('.').map(num => parseInt(num))
+    const addVersion = (arr, str = '') => {
+      if (arr.length == 1) {
+        return arr[0] + 1 + str
+      }
+      arr[arr.length - 1] = arr[arr.length - 1] + 1
+      if (arr[arr.length - 1] < 10) {
+        return arr.join('.') + str
+      } else {
+        arr.pop()
+        str += '.0'
+        return addVersion(arr, str)
+      }
     }
-    arr[arr.length - 1] = arr[arr.length - 1] + 1
-    if (arr[arr.length - 1] < 10) {
-      return arr.join('.') + str
-    } else {
-      arr.pop()
-      str += '.0'
-      return addVersion(arr, str)
-    }
+    packageJSON.version = addVersion(tep)
+    await writeJson(packageJSONPath, packageJSON, { spaces: 2 })
+    execSync('git add ./package.json && git commit -m "version update"', { cwd: process.cwd() })
+  } else if (name == 'master') {
+    execSync(`git tag ${version}`)
   }
-
-  packageJSON.version = addVersion(tep)
-  await writeJson(packageJSONPath, packageJSON, { spaces: 2 })
-
-  exec('git add ./package.json && git commit -m "version update"', { cwd: process.cwd() }, (error, stdout, stderr) => {
-    if (error) {
-      console.error(error)
-      process.exit(1)
-    }
-  })
-
 }
 
 run()
